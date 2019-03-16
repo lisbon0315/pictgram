e5c2e3d3b1ef5c341884290aa98a02fc2eea8d83
lisbon <lisbon0315666@gmail.com>
2019-03-16 12:57:25 +0900

    user.rb

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {in: 1..15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  validates :password, length: {in: 8..32}, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  has_secure_password
+end
